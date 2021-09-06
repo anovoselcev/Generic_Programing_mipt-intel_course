7fde69bd8bc96862c92979f6519369d2a93ccc0d
smileamaxdev <novoseltsev.as@phystech.edu>
2021-09-06 21:45:09 +0300

    write generic split

--- 1_Strings/utility.hpp
+++ 1_Strings/utility.hpp
@@ -1,21 +1,22 @@
 #pragma once
 #include <vector>
-#include <cow_string.hpp>
+
 namespace my {
 
 
 template<typename CharT,
          typename TraitsT,
-         typename Allocator>
-std::vector<my::cow_base_string<CharT, TraitsT, Allocator>> split(const my::cow_base_string<CharT, TraitsT, Allocator>& str,
-                                                                  CharT sep){
+         typename Allocator,
+         template<typename, typename, typename >typename StringT>
+std::vector<StringT<CharT, TraitsT, Allocator>> split(const StringT<CharT, TraitsT, Allocator>& str,
+                                                      CharT sep){
     size_t size = str.count(sep) + 1;
-    std::vector<my::cow_base_string<CharT, TraitsT, Allocator>> res(size);
+    std::vector<StringT<CharT, TraitsT, Allocator>> res(size);
     auto it = str.cbegin();
     size_t idx = 0;
     while(std::distance(it, str.cend()) > 0){
         const auto end = str.find(it, str.cend(), sep);
-        my::cow_base_string<CharT, TraitsT, Allocator> sub;
+        StringT<CharT, TraitsT, Allocator> sub;
         sub.assign(it, end);
         res[idx++] = std::move(sub);
         if(end == str.cend()) break;
